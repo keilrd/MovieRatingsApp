67549a5837a3aad608000eb0cb6b18505a414342
mohammadislam <mohammadislam@localhost>
2021-07-31 15:34:11 -0500

    adding genre sp

---
+++ MovieRatings/Stored Procedures/GetByGenre.sql
@@ -0,0 +1,33 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS GetByGenre;
+CREATE PROCEDURE GetByGenre(
+		IN favortiteGenre VARCHAR(255)
+	)
+BEGIN
+	Select DISTINCT 
+		m.TITLE 
+        ,m.`YEAR`
+        ,d.DIR_NAME 
+        ,m.CRITIC_RATE
+        ,m.MOV_ID
+        ,m.AUD_RATE
+        ,m.DIR_ID
+        ,m.AUD_COUNT
+    FROM 
+		MOVIES m
+        ,MOVIES_GENRE mg 
+        ,DIRECTORS d
+    Where
+		mg.GENRE LIKE CONCAT('%',LOWER(favortiteGenre),'%')
+        AND
+        mg.MOV_ID = m.MOV_ID
+        AND
+        m.DIR_ID = d.DIR_ID
+	ORDER BY 
+		m.CRITIC_RATE DESC
+	LIMIT 15;
+END //
+
+DELIMITER ;
+
